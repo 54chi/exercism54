7b825841cf1687755ffa5fc06b8c4223fdc247b0
54chi <zerocode001@gmail.com>
2016-04-11 10:17:34 -0500

    RLE

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -7,25 +7,38 @@ defmodule RunLengthEncoder do
   "1H1O1R1S1E" => "HORSE"
   """
 
-  def count(chars) do
-    # Either use the enum/map or an each
-    Enum.reduce(chars, %{}, &update_count/2)
-  end
+ def count_chunks(c, acc) do
+    count = Enum.count(c)
+    |> Integer.to_string
 
-   def update_count(word, acc) do
-       Map.update acc, word, 1, &(&1 + 1)
-     end
+    first_letter= List.first(c)
+    "#{acc}#{count}#{first_letter}"
+  end
 
   @spec encode(String.t) :: String.t
   def encode(string) do
     string
     |> String.codepoints
-    |> count
-  #  |> Enum.join
+    |> Enum.chunk_by(&(&1)) #divide it in chunks of repeating characters
+    |> Enum.reduce("", fn(c, acc) -> count_chunks(c, acc) end)
+  end
+
+  def print_chunks(c, acc) do
+    #get the number of letters
+    chunked=c
+    |> String.replace(~r/\d+/,"\\0 \\1")
+    |> String.split
+    letters=String.duplicate(List.last(chunked),String.to_integer(List.first(chunked)))
+    "#{acc}#{letters}"
   end
 
   @spec decode(String.t) :: String.t
   def decode(string) do
-
+    #\d+ identifies all numbers in a regex
+    #\d+[A-Z] will split in #s followed by any letter
+    string
+    |> String.replace(~r/\d+[A-Z]/,"\\0 ")
+    |> String.split
+    |> Enum.reduce("", fn(c, acc) -> print_chunks(c, acc) end)
   end
 end
